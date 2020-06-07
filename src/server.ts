3e3f5ad0604a9a92e750a46407b050f094d447c0
Wesley Rodrigues <wesleysilvarodrigues25@gmail.com>
2020-06-07 18:20:12 -0300

    Fix create points

--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,13 @@
 import express, { request, response } from 'express';
-
+import cors from 'cors';
 import path from 'path';
 import routes from './routes';
 
  const app = express();
 
+ app.use(cors());
  app.use(express.json()); 
  app.use(routes);
  app.use('/tmp', express.static(path.resolve(__dirname, '..', 'tmp')));
 
  app.listen(3333);
-
- /*app.get('/users/:id', (request, response)=> {
-     const id = Number(request.params.id);
-
-     const user = users[id];
-
-     return response.json(user);
- });
-
- app.post('/users', (request, response)=>{
-    const data = request.body;
-
-    console.log(data);
-     const user = {
-         name: data.name,
-         email: data.email
-     };
-
-     return response.json(user);
- });*/
